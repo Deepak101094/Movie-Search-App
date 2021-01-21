58ec001c3f8ae61e6286edf8b578f92dfbbe2751
Deepak101094 <deepakpandey101094@gmail.com>
2021-01-22 01:17:52 +0530

    design navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 //import './App.css';
 import MovieSearch from "./components/MovieSearch";
+import SearchIcon from "@material-ui/icons/Search";
+import MenuIcon from '@material-ui/icons/Menu';
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
-    <div className="container">
-      <h1 className="title">React Movie Application</h1>
-     <MovieSearch />
+    <div>
+    <Navbar />
+      <div className="container">
+        <MovieSearch />
+      </div>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <nav>
+        <div className="navbar-inner">
+          <div className="leftBar">
+            <div className="logo"><b>IMDB</b></div>
+            <div className="menuIcon">
+              <ion-icon
+                style={{ fontSize: "20px" }}
+                name="menu-outline"
+              ></ion-icon>
+              Menu
+            </div>
+          </div>
+
+          <div className="searchBox">
+            <input type="search" placeholder="search here" />
+            <span>
+              <ion-icon name="search-outline"></ion-icon>
+            </span>
+          </div>
+          <ol>
+            <li>
+              <a href="#">IMDBpro</a>
+            </li>
+            <li style={{textAlign: "center"}}>
+              <a href="#" > <ion-icon name="add-outline"></ion-icon> Watchlist</a>
+            </li>
+            <li>
+              <a href="#">Signin</a>
+            </li>
+          </ol>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
