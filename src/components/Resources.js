43816806c3ae26df226f7bf2d4a546ec3cb30591
Deepak101094 <deepakpandey101094@gmail.com>
2021-01-25 21:43:29 +0530

    remove inline styling

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -6,76 +6,74 @@ function Resources({ resources }) {
   const {
     Title,
     Actors,
-    Awards,
     Writer,
     Director,
     Poster,
     Plot,
     Released,
     imdbRating,
+    Genre,
+    Year,
+    Country,
   } = resources;
   return (
     <div className="container">
-       <div className="card-top">
-         <nav>
-         <a href="#">FULLCAST AND GREW</a>
+      <div className="card-top">
+        <nav>
+          <a href="#">FULLCAST AND GREW</a>
           <a href="#">TRIVIA </a>
           <a href="#">USER REVIEW</a>
           <a href="#">IDMbpro</a>
           <a href="#">MORE</a>
           <div className="share-button">
-          <a href="#">SHARE</a>
+            <a href="#">SHARE</a>
           </div>
-         </nav>
-         <div className="card-inner-div1">
-           <nav>
-           <ion-icon name="add-outline" style= {{fontSize: "30px"}} />
-          <h1 style={{fontSize: "40px",fontFamily: "serif"}}>Cloud Atlas (2021) </h1>
-           </nav>
-           <div style={{display:'flex',justifyContent: "space-between",marginLeft:"60px", padding:0}}>
-           <p>R</p>
-           <p>2h 52min</p>
-           <p>Action ,Drama,Mystery</p>
-           <p>26 october 2012 USA</p>
-           </div>       
-           <div>
-         
-           </div>
+        </nav>
+
+        <div className="card-inner-div1">
+          <nav>
+            <ion-icon name="add-outline" style={{ fontSize: "30px" }} />
+            <h1>
+              {Title} ({Year}){" "}
+            </h1>
+        <div className="rating"><ion-icon name="star-outline"></ion-icon> <p>{imdbRating}</p>  </div>
+
+          </nav>
+          <div className="details">
+            <p>R |</p>
+            <p>2h 52min |</p>
+            <p> {Genre} |</p>
+            <p>
+              {Released} {Country}{" "}
+            </p>
           </div>
+          <div></div>
         </div>
-      <div className="card">
-      <ReactImageFallback
-        src={Poster}
-        fallbackImage={DefaultImg}
-        initialImage="loader.gif"
-        alt="cool image should be here"
-        className="my-image"
-      />
-      <div className="card-content">
-        <p className="card-description"> {Plot} </p>
-        <p>
-          <small>Release Date: {Released} </small>
-        </p>
-        <p>
-          <small>Actors: {Actors} </small>
-        </p>
-        <p>
-          <small>Director: {Director} </small>
-        </p>
-        <p>
-          <small>Writer:{Writer} </small>
-        </p>
-        <p>
-          <small>Rating: {imdbRating} </small>
-        </p>
-        <p>
-          <small>Awards:{Awards} </small>
-        </p>
       </div>
-    </div>
-    
-      
-      
+      <div className="card">
+        <ReactImageFallback
+          src={Poster}
+          fallbackImage={DefaultImg}
+          initialImage="loader.gif"
+          alt="cool image should be here"
+          className="my-image"
+        />
+        <div className="card-content">
+          <p className="card-description"> {Plot} </p>
+
+          <p>
+            <b>Director:</b> <small>{Director} </small>
+          </p>
+          <p>
+            <b>Writer:</b>
+            <small> {Writer} </small>
+          </p>
+          <p>
+            <b>Actors:</b>
+            <small>{Actors} </small>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
